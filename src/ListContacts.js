29cf850c1fda3c94d6af3fedf3411c64572df010
Qingyuan Ma <qm28@georgetown.edu>
2020-03-20 14:38:05 -0400

    change the normal class component to the functional component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,36 +1,66 @@
 import React, { Component } from 'react';
 
-class ListContacts extends Component {
-     render(){
-         console.log("Props", this.props.contacts)
-         return(
-             <ol className='contact-list'>
-                {this.props.contacts.map((contact) => (
-                    <li key={contact.id} className="contact-list-item">
-                        <div 
-                        className="contact-avatar"
-                        // fist pair of curly braces for JS and second one for the object
-                        style={{
-                            backgroundImage: `url(${contact.avatarURL})`
-                        }}>
-                        </div>
 
-                        <div className="contact-details">
-                            <p>{contact.name}</p>
-                            <p>{contact.handle}</p>
-                        </div>
+function ListContacts(props){
+     return(
+        <ol className='contact-list'>
+        {props.contacts.map((contact) => (
+            <li key={contact.id} className="contact-list-item">
+                <div 
+                className="contact-avatar"
+                // fist pair of curly braces for JS and second one for the object
+                style={{
+                    backgroundImage: `url(${contact.avatarURL})`
+                }}>
+                </div>
 
-                        <button className="contact-remove">
-                            Remove
-                        </button>
+                <div className="contact-details">
+                    <p>{contact.name}</p>
+                    <p>{contact.handle}</p>
+                </div>
+
+                <button className="contact-remove">
+                    Remove 
+                </button>
+                
+            </li>
+        ))
+        }
+     </ol>
+     )
+}; 
+
+// class ListContacts extends Component {
+//      render(){
+//          console.log("Props", this.props.contacts)
+//          return(
+//              <ol className='contact-list'>
+//                 {this.props.contacts.map((contact) => (
+//                     <li key={contact.id} className="contact-list-item">
+//                         <div 
+//                         className="contact-avatar"
+//                         // fist pair of curly braces for JS and second one for the object
+//                         style={{
+//                             backgroundImage: `url(${contact.avatarURL})`
+//                         }}>
+//                         </div>
+
+//                         <div className="contact-details">
+//                             <p>{contact.name}</p>
+//                             <p>{contact.handle}</p>
+//                         </div>
+
+//                         <button className="contact-remove">
+//                             Remove 
+//                         </button>
                         
-                    </li>
-                ))
-                }
-             </ol>
-         )
+//                     </li>
+//                 ))
+//                 }
+//              </ol>
+//          )
 
-     }
-};
+//      }
+// };
 
 export default ListContacts;
