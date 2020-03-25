69549c67cb0d21f1863f1e9aad958e0bcad7f928
Qingyuan Ma <qm28@georgetown.edu>
2020-03-25 10:59:06 -0400

    add a new state called screen to control the div

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import ListContacts from "./ListContacts";
 import * as ContactsAPI from "./utils/ContactsAPI"
+import CreateContact from "./CreateContact";
+
+
 
 class App extends Component {
   state = {
@@ -23,7 +26,8 @@ class App extends Component {
       //   "handle": "tylermcginnis",
       //   "avatarURL": "http://localhost:5001/tyler.jpg"
       // }
-     ]
+     ],
+     screen: "create"
   }
   componentDidMount(){
     ContactsAPI.getAll()
@@ -46,10 +50,16 @@ class App extends Component {
   render() {
     return (
       <div>
-        <ListContacts 
+        {this.state.screen === "list" && 
+          <ListContacts 
           contacts={this.state.contacts}
-          onDeleteContact={this.removeContact}/>
+          onDeleteContact={this.removeContact}/>}
+        
+        {this.state.screen === "create" &&
+          <CreateContact />}
+        
       </div>
+      
     )
   }
 }
